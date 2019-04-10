ed74aa282cedebe3c90d9bb6c8561812dbee76b5
mohanson <mohanson@outlook.com>
2019-04-10 19:20:08 +0800

    2019-04-10 19:20:08

--- src/main.rs
+++ src/main.rs
@@ -29,17 +29,29 @@ impl AppState {
     }
 }
 
+// fn rpc_peer_count(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Error = Error> {
+// }
+
 fn echo(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Error = Error> {
     req.payload()
         .concat2()
         .from_err()
         .and_then(move |body| {
-            info!("{:?}", req.state().network.peer_count());
             let result = json::parse(std::str::from_utf8(&body).unwrap());
+            println!("{:?}", result);
             let injson: JsonValue = match result {
                 Ok(v) => v,
                 Err(e) => object! {"err" => e.to_string() },
             };
+            let obj = match injson {
+                JsonValue::Object(ref obj) => obj,
+                _ => panic!("No entry"),
+            };
+            info!("{:?}", obj.get("jsonrpc").unwrap());
+            info!("{:?}", obj.get("method").unwrap());
+            info!("{:?}", obj.get("params").unwrap());
+            info!("{:?}", obj.get("id").unwrap().as_i32().unwrap());
+
             Ok(HttpResponse::Ok()
                 .content_type("application/json")
                 .body(injson.dump()))
