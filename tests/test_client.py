dca66d7fb08b98df7ef267ba5aa98753b5680fc8
mohanson <mohanson@outlook.com>
2019-04-12 09:52:48 +0800

    2019-04-12 09:52:47

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,7 +3,7 @@ import requests
 r = requests.post('http://127.0.0.1:8080/', json={
     'jsonrpc': '2.0',
     'method': 'ping',
-    'params': ['Hello'],
+    'params': [],
     'id': 1
 })
 print(r.json())
