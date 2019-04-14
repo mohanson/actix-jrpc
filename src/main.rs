846a1851f0d8500e3acaa4456e18e8b1b380eec2
mohanson <mohanson@outlook.com>
2019-04-14 20:40:48 +0800

    2019-04-14 20:40:48

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::error;
+use std::sync::Arc;
+use std::sync::RwLock;
+use std::time::Duration;
+
 use actix_web::http::Method;
 use actix_web::{
     middleware, server, App, AsyncResponder, Error, HttpMessage, HttpRequest, HttpResponse,
@@ -6,14 +11,12 @@ use futures::{future, Future, Stream};
 use futures_timer::Delay;
 use serde_json;
 use serde_json::Value;
-use std::error;
-use std::sync::Arc;
-use std::time::Duration;
 
 #[allow(dead_code)]
 mod convention;
 
-fn rpc_main(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Error = Error> {
+/// The main handler for JSONRPC server.
+fn rpc_handler(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Error = Error> {
     req.payload()
         .concat2()
         .from_err()
@@ -32,27 +35,14 @@ fn rpc_main(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Erro
                         .body(r.dump()));
                 }
             };
-
             let app_state = req.state();
             let mut result = convention::Response::default();
             result.id = reqjson.id.clone();
 
-            match reqjson.method.as_str() {
-                "ping" => {
-                    let r = app_state.network.ping();
-                    result.result = Value::from(r);
-                }
-                "wait" => match app_state.network.wait(4).wait() {
-                    Ok(ok) => result.result = Value::from(ok),
-                    Err(e) => {
-                        result.error =
-                            Some(convention::ErrorData::new(500, &format!("{:?}", e)[..]))
-                    }
-                },
-                _ => {
-                    result.error = Some(convention::ErrorData::std(-32601));
-                }
-            };
+            match rpc_select(&app_state, reqjson.method.as_str()) {
+                Ok(ok) => result.result = ok,
+                Err(e) => result.error = Some(e),
+            }
 
             Ok(HttpResponse::Ok()
                 .content_type("application/json")
@@ -61,12 +51,45 @@ fn rpc_main(req: HttpRequest<AppState>) -> impl Future<Item = HttpResponse, Erro
         .responder()
 }
 
+fn rpc_select(app_state: &AppState, method: &str) -> Result<Value, convention::ErrorData> {
+    match method {
+        "ping" => {
+            let r = app_state.network.read().unwrap().ping();
+            return Ok(Value::from(r));
+        }
+        "wait" => match app_state.network.read().unwrap().wait(4).wait() {
+            Ok(ok) => return Ok(Value::from(ok)),
+            Err(e) => return Err(convention::ErrorData::new(500, &format!("{:?}", e)[..])),
+        },
+        "get" => {
+            let r = app_state.network.read().unwrap().get();
+            return Ok(Value::from(r));
+        }
+        "inc" => {
+            app_state.network.write().unwrap().inc();
+            return Ok(Value::Null);
+        }
+        _ => return Err(convention::ErrorData::std(-32601)),
+    }
+}
+
 pub trait ImplNetwork {
     fn ping(&self) -> String;
     fn wait(&self, d: u64) -> Box<Future<Item = String, Error = Box<error::Error>>>;
+
+    fn get(&self) -> u32;
+    fn inc(&mut self);
 }
 
-pub struct ObjNetwork {}
+pub struct ObjNetwork {
+    c: u32,
+}
+
+impl ObjNetwork {
+    fn new() -> Self {
+        Self { c: 0 }
+    }
+}
 
 impl ImplNetwork for ObjNetwork {
     fn ping(&self) -> String {
@@ -80,15 +103,23 @@ impl ImplNetwork for ObjNetwork {
         };
         Box::new(future::ok(String::from("pong")))
     }
+
+    fn get(&self) -> u32 {
+        self.c
+    }
+
+    fn inc(&mut self) {
+        self.c += 1;
+    }
 }
 
 #[derive(Clone)]
 pub struct AppState {
-    network: Arc<ImplNetwork>,
+    network: Arc<RwLock<ImplNetwork>>,
 }
 
 impl AppState {
-    pub fn new(network: Arc<ImplNetwork>) -> Self {
+    pub fn new(network: Arc<RwLock<ImplNetwork>>) -> Self {
         Self { network: network }
     }
 }
@@ -97,14 +128,14 @@ fn main() {
     std::env::set_var("RUST_LOG", "info");
     env_logger::init();
 
-    let network = Arc::new(ObjNetwork {});
+    let network = Arc::new(RwLock::new(ObjNetwork::new()));
 
     let sys = actix::System::new("actix_jrpc");
     server::new(move || {
         let app_state = AppState::new(network.clone());
         App::with_state(app_state)
             .middleware(middleware::Logger::default())
-            .resource("/", |r| r.method(Method::POST).with_async(rpc_main))
+            .resource("/", |r| r.method(Method::POST).with_async(rpc_handler))
     })
     .bind("127.0.0.1:8080")
     .unwrap()
